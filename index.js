7e0dc0e836bd1c3e069c2374fe476d059c161b35
Nikita Sakunde <105559912+Nikitasakunde@users.noreply.github.com>
2022-11-26 16:32:08 +0530

    new commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express  = require("express");
 const bodyparser = require("body-parser");
 const path = require("path");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 
 const app = express();
